62d243a0883df6ced596a4457790c283fb0c0032
MOHANRAJE28 <120411157+MOHANRAJE28@users.noreply.github.com>
2023-07-14 00:20:37 +0530

    address map is add

--- app/src/main/java/com/example/procare/ElectricianListActivity.java
+++ app/src/main/java/com/example/procare/ElectricianListActivity.java
@@ -1,15 +1,32 @@
 
 package com.example.procare;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.util.Log;
-
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -18,13 +35,22 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ElectricianListActivity extends AppCompatActivity {
+public class ElectricianListActivity extends AppCompatActivity  implements OnMapReadyCallback {//implements OnMapReadyCallback
     RecyclerView recyclerView;
     ArrayList<User> userArrayList;
     MyAdapter myAdapter;
-    FirebaseFirestore db;
+    FirebaseFirestore fstore;
     ProgressDialog progressDialog;
+
+    double latitude;
+    double longitude;
+    //google map
+    private GoogleMap mMap;
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +63,24 @@ public class ElectricianListActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycleView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        db = FirebaseFirestore.getInstance();
+        fstore = FirebaseFirestore.getInstance();
         userArrayList = new ArrayList<User>();
         myAdapter = new MyAdapter(ElectricianListActivity.this, userArrayList);
         recyclerView.setAdapter(myAdapter);
+        //google map
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLastLocation();
+        latitude = currentLocation.getLatitude();
+        longitude = currentLocation.getLongitude();
         EventchangeLister();
+
+//0.1
     }
+
     private void EventchangeLister() {
-        db.collection("Users")
-                .whereEqualTo("Qualification", "electrician")
-                .orderBy("FullName", Query.Direction.ASCENDING)
+        fstore.collection("Users")
+                .whereEqualTo("Qualification", "electrician") // Filter requests by ProviderEmail field
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
                     @Override
                     public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -56,17 +90,113 @@ public class ElectricianListActivity extends AppCompatActivity {
                             Log.e("Firestore error", error.getMessage());
                             return;
                         }
-
                         userArrayList.clear(); // Clear the previous list
                         for (DocumentChange dc : value.getDocumentChanges()) {
                             if (dc.getType() == DocumentChange.Type.ADDED) {
                                 userArrayList.add(dc.getDocument().toObject(User.class));
                             }
                         }
+
                         myAdapter.notifyDataSetChanged();
                         if (progressDialog != null && progressDialog.isShowing())
                             progressDialog.dismiss();
                     }
                 });
     }
+
+        private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null){
+                    currentLocation =location;
+                    SupportMapFragment mapFragment = new SupportMapFragment();
+                    getSupportFragmentManager().beginTransaction().add(R.id.mapFragment, mapFragment).commit();
+                    mapFragment.getMapAsync( ElectricianListActivity.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(sydney).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        float zoomLevel = 15f; // Set your desired zoom level here
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == FINE_PERMISSION_CODE) {
+        } else {
+            Toast.makeText(this, "Location permission is denied allow the permission", Toast.LENGTH_SHORT).show();
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+
+        }
+    }
 }
+
+
+
+//google map
+//    private void getLastLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+//            return;
+//        }
+//        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+//
+//        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//                if(location != null){
+//                    currentLocation =location;
+//                    SupportMapFragment mapFragment = new SupportMapFragment();
+//                    getSupportFragmentManager().beginTransaction().add(R.id.mapFragment, mapFragment).commit();
+//                    mapFragment.getMapAsync( ElectricianListActivity.this);
+//                }
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("My Location"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        float zoomLevel = 15f; // Set your desired zoom level here
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel));
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if (requestCode == FINE_PERMISSION_CODE){
+//           }else {
+////                Toast.makeText(this,"Location permission is denied allow the permission",Toast.LENGTH_SHORT).show();
+////              if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                getLastLocation();
+//           }
+//
+//        }
+//    }
+//
--- app/src/main/java/com/example/procare/User.java
+++ app/src/main/java/com/example/procare/User.java
@@ -2,15 +2,24 @@ package com.example.procare;
 
 public class User {
 
-    String FullName,PhoneNumber,Profilephoto,Qualification,UserEmail;
+    String FullName,PhoneNumber,Profilephoto,Qualification,UserEmail, uid;;
     public User(){}
 
-    public User(String fullName, String phoneNumber, String profilephoto ,String qualification,String userEmail) {
+    public User(String fullName, String phoneNumber, String profilephoto, String qualification, String userEmail, String uid) {
         FullName = fullName;
         PhoneNumber = phoneNumber;
         Profilephoto = profilephoto;
-        Qualification=qualification;
-        UserEmail=userEmail;
+        Qualification = qualification;
+        UserEmail = userEmail;
+        this.uid = uid;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 
     public String getFullName() {
@@ -53,4 +62,8 @@ public class User {
 
         Profilephoto = profilephoto;
     }
+
+//    public Object getUid() {
+//        return null;
+//    }
 }
