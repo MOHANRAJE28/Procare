762f2471db91b62f8b01b5e176b4b18d6cf8413e
MOHANRAJE28 <120411157+MOHANRAJE28@users.noreply.github.com>
2023-07-19 00:26:14 +0530

    spinner add

---
+++ app/src/main/java/com/example/procare/PlumberListActivity.java
@@ -0,0 +1,14 @@
+package com.example.procare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlumberListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plumber_list);
+    }
+}
